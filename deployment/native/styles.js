e4495bf00dab5690d1e00eae9e8d5499a8e013b5
Bharathidasan S <bdasan286@gmail.com>
2022-11-18 15:06:20 +0530

    Added the eml file generation code

---
+++ deployment/native/styles.js
@@ -0,0 +1,11 @@
+import * as style0 from "E:/Applications/EMLFileGeneration-main_2/themesource/administration/native/main";
+import * as style1 from "E:/Applications/EMLFileGeneration-main_2/themesource/nanoflowcommons/native/main";
+import * as style2 from "E:/Applications/EMLFileGeneration-main_2/themesource/webactions/native/main";
+import * as style3 from "E:/Applications/EMLFileGeneration-main_2/themesource/atlas_core/native/main";
+import * as style4 from "E:/Applications/EMLFileGeneration-main_2/themesource/emlfilegeneration/native/main";
+import * as style5 from "E:/Applications/EMLFileGeneration-main_2/themesource/mymodule/native/main";
+import * as style6 from "E:/Applications/EMLFileGeneration-main_2/theme/native/main";
+
+import { flatten } from "mendix/native";
+
+module.exports = flatten([style0, style1, style2, style3, style4, style5, style6]);
